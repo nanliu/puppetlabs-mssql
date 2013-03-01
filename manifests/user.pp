0b4d24b193fc35d5b606a7a272fc46aa8a6e72d2
Nan Liu <nan.liu@gmail.com>
2013-02-28 18:33:18 -0800

    Add user management.

---
+++ manifests/user.pp
@@ -0,0 +1,55 @@
+define mssql::user (
+  $password,
+  $default_database,
+  $default_language = 'us_english',
+  $check_policy     = 'OFF',
+  $roles            = undef,
+  $sqlcmd           = '"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd.exe"',
+  $debug            = false,
+) {
+  require 'mssql'
+
+  $create_sql = inline_template("
+USE [<%= @default_database %>]
+CREATE LOGIN [<%= @name %>] WITH PASSWORD = N'<%= @password %>',
+DEFAULT_DATABASE = [<%= @default_database %>],
+DEFAULT_LANGUAGE = [<%= @default_language %>],
+CHECK_POLICY = <%= @check_policy %>
+CREATE USER [<%= @name %>] FOR LOGIN [<%= @name %>]
+GO")
+
+  exec { "create_user_${name}":
+    path      => $::path,
+    command   => "${sqlcmd} -Q \"${create_sql}\"",
+    unless    => "${sqlcmd} -Q \"exit(if exists(select * from master.sys.server_principals where name='${name}') select 0 else select 1)\"",
+    logoutput => true,
+    require   => Mssql::Db[$default_database],
+  }
+
+  if $roles {
+    $role_sql = inline_template("
+USE [<%= @default_database %>]
+GO
+<% @roles.each do |r| -%>
+sp_addrolemember [<%= r %>], [<%= @name %>]
+<% end -%>
+GO")
+
+    exec { "role_${name}":
+      path        => $::path,
+      command     => "${sqlcmd} -Q \"${role_sql}\"",
+      logoutput   => true,
+      refreshonly => true,
+      subscribe   => Exec["create_user_${name}"],
+    }
+  }
+
+  if $debug {
+    notify { "create_user_${name}":
+      message => $create_sql,
+    }
+    notify { "role_${name}":
+      message => $role_sql,
+    }
+  }
+}
