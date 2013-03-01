148f067e0e45c04f083f64fd233820e5a95567b0
Nan Liu <nan.liu@gmail.com>
2013-02-28 18:31:27 -0800

    Add database management.

---
+++ manifests/db.pp
@@ -0,0 +1,62 @@
+define mssql::db (
+  $sqlcmd     = '"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd.exe"',
+  $mdf_file   = undef,
+  $mdf_size   = '100MB',
+  $mdf_growth = '10%',
+  $ldf_file   = undef,
+  $ldf_size   = '10MB',
+  $ldf_growth = '10%',
+  $collate    = 'Latin1_General_CS_AS',
+  # These settings are configure on database creation and not enforced subsequently:
+  $settings   = undef,
+  $debug      = false,
+) {
+  require 'mssql'
+
+  $create_sql = inline_template("
+USE [master]
+CREATE DATABASE [<%= @name %>]
+<% if @mdf_file -%>
+ON PRIMARY
+(NAME = N'<%= @name %>', FILENAME = N'<%= @mdf_file %>', SIZE = <%= @mdf_size %>, FILEGROWTH = <%= @mdf_growth %> )
+<% end -%>
+<% if @ldf_file -%>
+LOG ON
+(NAME = N'<%= @name %>_log', FILENAME = N'<%= @ldf_file %>', SIZE = <%= @ldf_size %>, FILEGROWTH = <%= @ldf_growth %> )
+<% end -%>
+COLLATE <%= @collate %>
+GO")
+
+  exec { "create_db_${name}":
+    path      => $::path,
+    command   => "${sqlcmd} -Q \"${create_sql}\"",
+    unless    => "${sqlcmd} -Q \"exit(if exists(select * from master.sys.databases where name='${name}') select 0 else select 1)\"",
+    logoutput => true,
+  }
+
+  if $settings {
+    $alter_sql = inline_template("
+USE [<%= @name %>]
+<% @settings.each do |l| -%>
+ALTER DATABASE [<%= @name %>] <%= l %>;
+<% end -%>
+GO")
+
+    exec { "alter_${name}":
+      path        => $::path,
+      command     => "${sqlcmd} -Q \"${alter_sql}\"",
+      logoutput   => true,
+      refreshonly => true,
+      subscribe   => Exec["create_db_${name}"],
+    }
+  }
+
+  if $debug {
+    notify { "create_db_${name}":
+      message => $create_sql,
+    }
+    notify { "alter_${name}":
+      message => $alter_sql,
+    }
+  }
+}
